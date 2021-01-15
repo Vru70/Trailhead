11fd54dfcd37b2e28070d237371dae6ec201deaa
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-01-15 16:47:20 +0530

    parent child quotes

---
+++ force-app/main/default/lwc/childQuotes/childQuotes.js
@@ -0,0 +1,6 @@
+import { LightningElement , api} from 'lwc';
+
+export default class ChildQuotes extends LightningElement 
+{
+    @api quotes;
+}
---
+++ force-app/main/default/lwc/parentAllQuotes/parentAllQuotes.js
@@ -0,0 +1,25 @@
+import { LightningElement } from 'lwc';
+
+export default class ParentAllQuotes extends LightningElement 
+{
+    quotes = 
+    [
+        {
+            Id: 1,
+            quote : 'aaa',
+        },
+        {
+            Id: 2,
+            quote : 'aaa',
+        },
+        {
+            Id: 3,
+            quote : 'aaa',
+        },
+        {
+            Id: 4,
+            quote : 'aaa',
+        },
+
+    ];
+}
