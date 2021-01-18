38700607709f7ccb96e70cc69c76e67e048eb25a
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-01-18 21:25:31 +0530

    LMS app

--- force-app/main/default/lwc/lMSChild/lMSChild.js
+++ force-app/main/default/lwc/lMSChild/lMSChild.js
@@ -6,10 +6,16 @@ export default class LMSChild extends LightningElement
     {
         name:'Salesforce Lightning Administrator',
         duration: '30 Days',
-        fees: 'FREE',
+        fees: '5000',
         trainer: 'Vrushbah',
         rating:'4',
         img: 'https://img-a.udemycdn.com/course/240x135/3703496_2953_3.jpg',
-    }
+        id: 1,
+    };
+
+    @api test={
+        id:1,
+        result:'resuk set',
+    };
 
 }
